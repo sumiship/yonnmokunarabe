e78bd9202f267cf167107fa6f9bea1df16c9c4c7
sumiship <osumi.k.osumi.k@gmail.com>
2020-12-23 00:02:25 +0900

    playerの表示

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@
       }
       document.getElementById('1-1').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('2').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -22,6 +23,7 @@
       }
       document.getElementById('1-2').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('3').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -32,6 +34,7 @@
       }
       document.getElementById('1-3').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('4').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -42,6 +45,7 @@
       }
       document.getElementById('1-4').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('5').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -52,6 +56,7 @@
       }
       document.getElementById('1-5').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('6').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -62,6 +67,7 @@
       }
       document.getElementById('1-6').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('7').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -72,6 +78,7 @@
       }
       document.getElementById('1-7').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('8').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -82,6 +89,7 @@
       }
       document.getElementById('1-8').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('9').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -92,6 +100,7 @@
       }
       document.getElementById('1-9').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('10').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -102,6 +111,7 @@
       }
       document.getElementById('1-10').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('11').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -112,6 +122,7 @@
       }
       document.getElementById('1-11').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('12').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -122,6 +133,7 @@
       }
       document.getElementById('1-12').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('13').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -132,6 +144,7 @@
       }
       document.getElementById('1-13').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('14').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -142,6 +155,7 @@
       }
       document.getElementById('1-14').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('15').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -152,6 +166,7 @@
       }
       document.getElementById('1-15').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('16').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -162,6 +177,7 @@
       }
       document.getElementById('1-16').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('17').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -172,6 +188,7 @@
       }
       document.getElementById('1-17').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('18').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -182,6 +199,7 @@
       }
       document.getElementById('1-18').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('19').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -192,6 +210,7 @@
       }
       document.getElementById('1-19').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('20').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -202,6 +221,7 @@
       }
       document.getElementById('1-20').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('21').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -212,6 +232,7 @@
       }
       document.getElementById('1-21').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('22').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -222,6 +243,7 @@
       }
       document.getElementById('1-22').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('23').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -232,6 +254,7 @@
       }
       document.getElementById('1-23').appendChild(coin);
       i += 1;
+      which();
     })
     document.getElementById('24').addEventListener('click', ()=>{
       const coin = document.createElement('div');
@@ -242,7 +265,18 @@
       }
       document.getElementById('1-24').appendChild(coin);
       i += 1;
+      which();
     })
+  }
+  function which(){
+    const p = document.querySelector('h1');
+    if (i%2===0) {
+      document.getElementById('123').className = 'player1';
+      p.textContent = 'Player1';
+    } else{
+      document.getElementById('123').className = 'player2';
+      p.textContent = 'Player2';
+    }
   }
   set();
 }
