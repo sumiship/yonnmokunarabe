77d36606f6fb6570c057ad5bf6bf027b6b65b135
sumiship <osumi.k.osumi.k@gmail.com>
2020-12-22 23:24:17 +0900

    Completed for the time being

---
+++ js/main.js
@@ -0,0 +1,254 @@
+'use strict'
+
+{
+  let i = 0;
+  function set(){
+    document.getElementById('1').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-1').appendChild(coin);
+      
+      console.log(i);
+    })
+    document.getElementById('2').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-2').appendChild(coin);
+      i += 1;
+      console.log(i);
+    })
+    document.getElementById('3').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-3').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('4').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-4').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('5').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-5').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('6').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-6').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('7').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-7').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('8').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-8').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('9').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-9').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('10').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-10').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('11').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-11').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('12').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-12').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('13').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-13').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('14').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-14').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('15').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-15').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('16').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-16').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('17').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-17').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('18').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-18').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('1').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin19';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-19').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('20').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-20').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('21').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-21').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('22').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-22').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('23').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-23').appendChild(coin);
+      i += 1;
+    })
+    document.getElementById('24').addEventListener('click', ()=>{
+      const coin = document.createElement('div');
+      if (i%2===0) {
+        coin.className = 'coin1';
+      } else{
+        coin.className = 'coin2';
+      }
+      document.getElementById('1-24').appendChild(coin);
+      i += 1;
+    })
+  }
+  set();
+}
+
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,7 @@
+'use strict'
+
+{
+  const player1 = storage.getItem('player1');
+  const player2 = storage.getItem('player2');
+
+}
